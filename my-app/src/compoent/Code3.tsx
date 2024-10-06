7a7415fe3090b0a27d6c7049ef88c2d37c868136
aniket2727 <aniketkadam5950@gmail.com>
2024-10-06 12:37:15 +0530

    add code

---
+++ my-app/src/compoent/Code3.tsx
@@ -0,0 +1,18 @@
+
+
+// emi calucalatores
+
+import { useState } from "react";
+
+
+const Code3=()=>{
+    const [data,setdata]=useState<object>({price:'',years:'',interestrate:''})
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+
+export default Code3;
