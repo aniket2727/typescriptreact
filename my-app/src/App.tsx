d1842b67c850ee188c7140f39bef0b7cc3671617
aniket2727 <aniketkadam5950@gmail.com>
2024-10-06 11:59:23 +0530

    Add my-app folder as a regular directory

---
+++ my-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+// echo "# react_with_typescript" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/aniket2727/react_with_typescript.git
+// git push -u origin main
